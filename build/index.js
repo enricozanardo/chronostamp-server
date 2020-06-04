12b184e651bef1834cc4aa173f1e520f610a3ba6
enrico <ezanardo@onezerobinary.com>
2020-06-04 11:13:06 +0200

    build application

--- build/index.js
+++ build/index.js
@@ -6,12 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const cors = require('cors');
 const routes_1 = __importDefault(require("./routes"));
+const config_1 = require("./config");
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.options('*', cors());
 app.use(routes_1.default);
-const port = 8181;
+const port = config_1.PORT || 30466;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });

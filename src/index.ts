7fac6c54b28ad08b67a20df57db674db3219642f
enrico <ezanardo@onezerobinary.com>
2020-06-11 14:34:30 +0200

    get files from endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import * as path from 'path';
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 import apiRouter from './routes';
 import { PORT } from './config';
 
 const app = express();
+// enable files upload
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.options('*', cors());
 app.use(apiRouter);
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,103 @@
 const express = require('express');
 const stripeLoader = require('stripe');
+const _ = require('lodash');
 
 import { STRIPE_PRIV_KEY } from './config';
 
 const apiRouter = express.Router();
 
+enum Amount {
+  'Package1' = '5.00',
+  'Package2' = '10.00',
+  'Package3' = '50.00',
+}
+
+export enum TabsType {
+  wallet = 'WALLET',
+  assets = 'ASSETS',
+  search = 'SEARCH',
+}
+
+export type Profile = {
+  chronoStampID: string;
+  email: string;
+  account: string;
+  balance: number;
+  fullName?: string;
+  picture?: string;
+  sub?: string;
+  tab: TabsType;
+};
+
+const fakeUser: Profile = {
+  chronoStampID: '',
+  email: '',
+  account: '',
+  balance: 0,
+  tab: TabsType.wallet,
+};
+
 apiRouter.get('/api/hello', (req: any, res: any, next: any) => {
-  res.json('world');
+  res.json(fakeUser);
+});
+
+apiRouter.post('/api/profile', (req: any, res: any, next: any) => {
+  console.log(req.body);
+
+  fakeUser.chronoStampID = req.body.chronoStampID;
+  fakeUser.email = req.body.email;
+  fakeUser.fullName = req.body.fullName;
+  fakeUser.picture = req.body.picture;
+  fakeUser.sub = req.body.sub;
+  fakeUser.tab = req.body.tab;
+
+  res.status(200).send(fakeUser);
+  // res.json(fakeUser);
+});
+
+apiRouter.post('/api/account', (req: any, res: any, next: any) => {
+  console.log(req.body);
+
+  // TODO: based on the ID check and do the operations
+  // fakeUser.chronoStampID = req.body.chronoStampID;
+
+  if (fakeUser.account === '') {
+    fakeUser.account = 'TBMXSZXAEK7X6JC4XB7R5Y4JGPWNBALTBTYV4KAK';
+  }
+
+  res.status(200).send(fakeUser);
+});
+
+apiRouter.post('/api/asset', async (req: any, res: any, next: any) => {
+  try {
+    if (!req.files) {
+      return res.status(500).send({ msg: 'file is not found' });
+    }
+    // accessing the file
+    let myFile = req.files.asset;
+    let originalTitle = myFile.name;
+    let size = myFile.size;
+    console.log(originalTitle);
+    console.log(size);
+
+    await myFile.mv(`../uploaded/${originalTitle}`);
+
+    // TODO: based on the ID check and do the operations
+    // fakeUser.chronoStampID = req.body.chronoStampID;
+
+    res.status(200).send(fakeUser);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+apiRouter.post('/api/update', (req: any, res: any, next: any) => {
+  console.log(req.body);
+  fakeUser.account = req.body.account;
+  fakeUser.balance = req.body.balance;
+  fakeUser.chronoStampID = req.body.chronoStampID;
+
+  res.status(200).send(true);
 });
 
 const stripe = new stripeLoader(STRIPE_PRIV_KEY);
@@ -19,7 +110,7 @@ const charge = (token: string, amount: string, chronoStampID: string) => {
     amount: new_amount,
     currency: CURRENCY,
     source: token,
-    description: `Package One ordered. ${chronoStampID}`,
+    description: `Package ordered. ${chronoStampID}`,
   });
 };
 
@@ -31,9 +122,9 @@ apiRouter.post('/api/payment', async (req: any, res: any, next: any) => {
     let data = await charge(
       req.body.token.id,
       req.body.amount,
-      req.body.chronoStampID
+      req.body.profile.chronoStampID
     );
-    console.log(data);
+
     res.send('Charged');
   } catch (err) {
     console.log(`It was not possible to connect to Stripe. ${err}`);
